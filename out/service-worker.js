ac36eaec916ac7d9869def572ef4145490195ddb
Yuris594 <yurisbolanos594@gmail.com>
2025-01-21 09:39:02 -0500

    cambios

---
+++ out/service-worker.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'MG-MOVIL';
+const STATIC_ASSETS = [
+    '/',
+    '/index.html/',
+    '/pages.html/',
+    '/client.html/',
+    '/client/pedidos.html/',
+    '/pedidoSinEnviar.html/',
+    '/cartera.html/',
+    '/inventario.html/',
+    '/client/cotizacion.html/',
+    '/cotizacion.html/',
+    '/favicon.ico',
+    '/LOGO.png',
+    '/globals.css',
+];
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Instalado');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Archivos en caché');
+            return cache.addAll(STATIC_ASSETS);
+        }).catch((error) => {
+            console.error('Error al agregar archivos al caché:', error);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activado');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service Worker: Eliminando caché antigua', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Interceptando solicitud:', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request)
+                .then((response) => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    const responseClone = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseClone);
+                    });
+
+                    return response;
+                })
+                .catch(() => {
+                    if (event.request.headers.get('accept')?.includes('text/html')) {
+                        return caches.match('/');
+                    }
+                });
+        })
+    );
+});
--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register('/service-worker.js')
       .then(reg => console.log('Service Worker registrado:', reg))
       .catch(err => console.error('Error al registrar el Service Worker:', err));
   });
